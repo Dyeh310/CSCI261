e161b93e1936807d143bc35855509a9f3a0b505a
Dyeh310 <nheyd9292@gmail.com>
2018-01-27 18:21:54 -0500

    Create HelloWorld.cpp
    Classic Hello World with C++

---
+++ Project1/HelloWorld.cpp
@@ -0,0 +1,24 @@
+//Author: Nicholas Heyd
+/* This is my first C++ program
+   Csci 261, Spring 2018 */
+
+#include <iostream>
+
+using namespace std;
+int main() {
+	//outputs to the screen the following text:
+	cout << "Hello, World" <<endl;
+	cout << "This is my first C++ program" << endl;
+
+	//pauses the program so user can view the output before closing the application
+	system("pause");
+
+
+	/*Q & A:
+		
+		--Coments do not change the output of a program.
+		--If 'cout' is changed to 'Cout' the compiler produces an error message saying that 'Cout" is unidentified. C++ is case-sensitive.
+
+
+	*/
+}
